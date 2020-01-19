e73eb77ef025e1db84ad9cd2ce80c9a22fe33c1f
Hutch Crowley <tothecoda@gmail.com>
2020-01-19 05:31:03 -0600

    init commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<App />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(App />).toJSON()
+  
+  expect(tree).toMatchSnapshot();
+})
+})
